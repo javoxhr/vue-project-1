428d125421c3102c496f6305eeedef72f01ce796
javoxhr <javohiruzum@gmail.com>
2024-06-09 13:40:22 +0500

    zero

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,12 @@
 <template>
-  <div class="wrapper">
-    <h4 class="balance"> {{ count }} $</h4>
-    <h1 class="war">wraning</h1>
-    <span class="monet" @click="count ++">$</span>
-    <h1 class="title">Mashka coin</h1>
-    <h1 class="after-coin">After: <a href="https://t.me/notcoinchatuzbekistan">Umurbek@gmail.com</a> </h1>
+  <div>
+
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      count: 0,
-    }
-  }
+
 }
 </script>
 
